75a1d91e70d8553919afee996056131eaea3380c
sangliang <378305868@qq.com>
2016-10-18 16:09:28 +0800

    some changed

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var Hamster = Hamster || {};
 
-Hamster.spriteList = [];
+Hamster.spriteList = [];    //the list of sprites which need to be rended
 Hamster.ctx = null;
 Hamster.timeloop = null;
-Hamster.gameWidth =null;
+Hamster.gameWidth = null;
 Hamster.gameHeight = null;
+
 /**
  * main canvas,canvas data setting
  * @id {id}
@@ -12,7 +13,7 @@ Hamster.gameHeight = null;
  * @height {number}
  * @background {css color}
  */
-Hamster.init = function (id, width, height, timeloop, background) {
+Hamster.init = function(id, width, height, timeloop, background) {
 	var canvas = document.getElementById(id);
 	var ctx = canvas.getContext('2d');
 	Hamster.ctx = ctx;
@@ -27,9 +28,25 @@ Hamster.init = function (id, width, height, timeloop, background) {
 	canvas.style.margin = "0px auto";
 	canvas.style.display = "block";
 	canvas.style.boxShadow = "4px 4px 4px #888888";
+
 	// 开启游戏主循环
 	Hamster.setGameLoop();
 };
+
+Hamster.rendingStage = function(){
+	var self = this;
+	console.warn(Hamster.spriteList);
+	if(Hamster.spriteList.length==0){return;}
+
+	for(var i = 0;i<Hamster.spriteList.length;i++){
+		Hamster.spriteList[i].draw();
+	}
+}
+
+// rending sprite
+Hamster.rending = function(obj){
+	Hamster.ctx.drawImage(obj.texture, obj.x, obj.y, obj.width || obj.texture.height, obj.texture.height || obj.texture.weight);	
+}
 
 /**
  * Game sprite
@@ -38,7 +55,7 @@ Hamster.init = function (id, width, height, timeloop, background) {
  * @x {number}
  * @y {number}
  */
-Hamster.sprite = function (name, image, x, y) {
+Hamster.sprite = function(name, image, x, y) {
 	var self = this;
 	self.name = "name";
 	self.x = x || 0;
@@ -49,25 +66,27 @@ Hamster.sprite = function (name, image, x, y) {
 	self.texture = new Image();
 	self.texture.src = image;
 
-	self.draw = function () {
-		self.texture.onload = function () {
-			Hamster.ctx.drawImage(self.texture, self.x, self.y, self.width || self.texture.height, self.texture.height || self.texture.weight);
-			Hamster.ctx.restore();
+	//first draw while textures loaded
+	self.draw = function() {
+		Hamster.rending(self);
+
+		self.texture.onload = function() {
+			Hamster.rending(self);
 		}
-		self.texture.onerror = function () {
+		self.texture.onerror = function() {
 			console.log(self.name + "image load error");
 		}
 	}
 
-	self.add = function (gameObj, _x, _y) {
+	self.add = function(gameObj, _x, _y) {
 		gameObj._parent = this.gameObj.name;
 	}
 
-	self.setPosition = function(m,n){
+	self.setPosition = function(m, n) {
 		self.x = m;
-		self.y =n;
-		Hamster.ctx.clearRect(0,0,Hamster.gameWidth,Hamster.gameHeight)
-		Hamster.ctx.drawImage(self.texture, self.x, self.y, self.width || self.texture.height, self.texture.height || self.texture.weight);
+		self.y = n;
+		Hamster.ctx.clearRect(0, 0, Hamster.gameWidth, Hamster.gameHeight);
+		Hamster.rending(self);
 		return self.x;
 	}
 
@@ -76,30 +95,32 @@ Hamster.sprite = function (name, image, x, y) {
 
 Hamster.freshList = {};
 
-Hamster.freshList.pushList = function (gameObj) {
+Hamster.freshList.pushList = function(gameObj) {
 	Hamster.spriteList.push(gameObj);
 }
 
 /**main loop */
 Hamster.timeInterval = null;
-Hamster.setGameLoop = function (callback) {
+Hamster.setGameLoop = function(callback) {
 	var self = this;
 	self.callback = callback;
-	Hamster.timeInterval = setInterval(function () {
-		if(self.callback){
+	Hamster.timeInterval = setInterval(function() {
+		if (self.callback) {
 			self.callback();
 		}
-	}, 1 / Hamster.timeloop);
+	}, 10 / Hamster.timeloop);
+
 }
 
-Hamster.removeGameLoop = function () {
+/**clear loop */
+Hamster.removeGameLoop = function() {
 	if (Hamster.timeInterval) {
 		clearInterval(Hamster.timeInterval);
 	}
 }
 
 /**add to stage */
-Hamster.add = function (gameObj, x, y) {
+Hamster.add = function(gameObj, x, y) {
 	var self = this;
 	self.x = x || gameObj.x;
 	self.y = y || gameObj.y;
@@ -111,37 +132,41 @@ Hamster.add = function (gameObj, x, y) {
 
 /*sprite animation*/
 Hamster.ani = {};
-Hamster.ani.moveDirect = function (obj, targetX, targetY, moveTime) {
+Hamster.ani.moveDirect = function(obj, targetX, targetY, moveTime) {
 	if (!obj || !targetX || !targetY || !moveTime) {
 		console.error("have error parm");
 		return;
 	}
 	var self = this;
-	
+	var _t = moveTime*10;
+	var _time = null;
 	var _speedX = (targetX - obj.x) / moveTime;
 	var _speedY = (targetY - obj.y) / moveTime;
 
 	var _x = 0;
 	var _y = 0;
-	var tick = function () {
+	var _tick = function() {
 		if (targetX - obj.x > 0) {
-			_x = obj.x+_speedX;
+			_x = obj.x + _speedX;
 		}
 		if (targetY - obj.y > 0) {
-			 _y = obj.y+_speedY;
+			_y = obj.y + _speedY;
 		}
-		if(targetX - obj.x <= 0 && targetY-obj.y<=0){
-			Hamster.removeGameLoop(tick);
+		if (targetX - obj.x <= 0 && targetY - obj.y <= 0) {
+			if (_time) {
+				clearInterval(_time);
+			}
 		}
-		obj.setPosition(_x,_y);
-		// console.log(obj.setPosition(_x,_y));
+		obj.setPosition(_x, _y);
+		Hamster.rendingStage();
 	}
-	Hamster.setGameLoop(tick);
-	tick();
+	_time = setInterval(function() {
+		_tick();
+	}, _t / Hamster.timeloop);
+
 }
 
-; +(function () {
+; + (function() {
 	Hamster.name = "仓鼠";
-	Hamster.start = function () {
-	};
+	Hamster.start = function() {};
 })();
--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var sa = new Hamster.sprite('sa',"./public/resource/1.jpg",0,0);
 Hamster.add(sa,0,0);
 var hero = new Hamster.sprite('hero',"./public/resource/curry.jpg",0,0);
 Hamster.add(hero,100,100);
-Hamster.ani.moveDirect(hero,500,500,1000);
+Hamster.ani.moveDirect(hero,500,500,60);
 console.dir(hero.texture);
 
 // var t = setInterval(function(){
